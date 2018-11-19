75341ed0b811924cda8aec19e0c7d374284ace0c
Вадим <32982540+VadimKNF@users.noreply.github.com>
2018-11-19 21:38:14 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <p>Выберите действие</p>
-<form action="enteradress/bilet.php" method="GET">
+<form action="https://easyfunc.herokuapp.com//bilet.php" method="GET">
 	<input type="submit" value="проверить билет">
 </form>
-<form action="enteradress/checkyear.php" method="GET">
+<form action="https://easyfunc.herokuapp.com//checkyear.php" method="GET">
 	<input type="submit" value="проверить год">
 </form>
-<form action="enteradress/howmuchbeforeNewYear.php" method="GET">
+<form action="https://easyfunc.herokuapp.com//howmuchbeforeNewYear.php" method="GET">
 	<input type="submit" value="количество дней до нового года">
 </form>
