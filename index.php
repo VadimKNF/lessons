78d689c662ed0c7384bf61c2dc1425713066ae90
Вадим <32982540+VadimKNF@users.noreply.github.com>
2018-11-19 21:00:10 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<p>Выберите действие</p>
+<form action="http://localhost/dashboard/lessons/bilet.php" method="GET">
+	<input type="submit" value="проверить билет">
+</form>
+<form action="http://localhost/dashboard/lessons/checkyear.php" method="GET">
+	<input type="submit" value="проверить год">
+</form>
+<form action="http://localhost/dashboard/lessons/howmuchbeforeNewYear.php" method="GET">
+	<input type="submit" value="количество дней до нового года">
+</form>
